217743a9115bde4f0b4384d8f840507852951f69
Saurabh81770 <techcroft.info@gmail.com>
2026-09-04 09:46:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,34 +3,34 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "352069"))
+API_ID = int(os.environ.get("API_ID", "39890249"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","35cea1c5e6384f57e914ac9ff5ffd1")
+API_HASH = os.environ.get("API_HASH","c08bc45cddc181bd56e4313aca1fefda")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8309873942:AAHKpbr7GhXOiupMfpdBeNSsF2jBdH1dg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8009811715:AAGnUBNPVsWCaO1dbOAOUogFYZ34y1Sjq78")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@extract_txt_new_bot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@VPSBOT99_bot")
 BOT_TEXT = "℘ཞıŋƈɛ℘ıą"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "59388712"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7867604125"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-10037678927"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003658601243"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003766078927")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003569626935")) 
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://princepiax:princea45@cluster0.4kn7hq0.mongodb.net/?appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://gladocelotkqaq_db_user:e4U0GHT8Dc2h67He@cluster0.ehjezrt.mongodb.net/?appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-10037660927"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "1000000"))
 # -----------------------------------------------
-join = '<a href="https://t.me/princepia">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/Grow_with_Saurabh">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
-UNSPLASH_QUERY = 'animal baby'
+UNSPLASH_QUERY = 'saurbah'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Princepia" #without @
+ADMIN_BOT_USERNAME = "tumhare papa ji" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://imgcdn.stablediffusionweb.com/2024/4/17/53a08e9d-950a-4c1b-8808-767ea2cea7e6.jpg")
 
